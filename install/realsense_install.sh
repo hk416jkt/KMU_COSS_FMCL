052bf0a89cdf162b569fa8baaf0fc81988536e35
Jun seong Park <125014941+hk416jkt@users.noreply.github.com>
2023-11-30 18:47:55 +0900

    Add files via upload

---
+++ install/realsense_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#this file is ssetting for intel realsense d435.
+# recommand version : ubuntu 22.04
+# recommand HW : realsense d435
+
+sudo mkdir -p /etc/apt/keyrings
+curl -sSf https://librealsense.intel.com/Debian/librealsense.pgp | sudo tee /etc/apt/keyrings/librealsense.pgp > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/librealsense.pgp] https://librealsense.intel.com/Debian/apt-repo `lsb_release -cs` main" | \
+sudo tee /etc/apt/sources.list.d/librealsense.list
+sudo apt-get update
+sudo apt-get install librealsense2-dkms
+sudo apt-get install librealsense2-utils
+sudo apt-get install librealsense2-dev
+sudo apt-get install librealsense2-dbg
