25dc7f439e51ca340444b2b4071372db2e6791da
Jun seong Park <125014941+hk416jkt@users.noreply.github.com>
2024-01-08 22:39:35 +0900

    Update Coss_ROS2_install_rasp.sh

--- install/Coss_ROS2_install_rasp.sh
+++ install/Coss_ROS2_install_rasp.sh
@@ -54,7 +54,7 @@ alias cw='cd ~/ros2_ws'
 alias cs='cd ~/ros2_ws/src'
 alias cb='cd ~/ros2_ws && colcon build --symlink-install'
 alias eb='nano ~/.bashrc'
-alias sb='source ~/.bashrc'"
+alias sb='source ~/.bashrc'" >> ~/.bashrc
 
 mkdir -p ~/ros2_ws/src
 cd ~/ros2_ws/src
