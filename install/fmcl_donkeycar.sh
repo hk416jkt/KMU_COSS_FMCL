4ee67122fdf9d2e8c2f711d4d3ae47d7ba5e761a
Jun seong Park <125014941+hk416jkt@users.noreply.github.com>
2024-02-01 23:38:30 +0900

    Update fmcl_donkeycar.sh

--- install/fmcl_donkeycar.sh
+++ install/fmcl_donkeycar.sh
@@ -3,34 +3,35 @@
 # maker: Jun seong Park
 # donkey car install on RPi 5
 
-sudo apt update && sudo apt upgrade
-sudo apt install libffi-dev cmake git
-sudo apt-get install python3-launchpadlib
-sudo apt install software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.9
+sudo apt-get update --allow-releaseinfo-change
+sudo apt-get upgrade
 
 python3 -m venv env --system-site-packages
 echo "source ~/env/bin/activate" >> ~/.bashrc
 source ~/.bashrc
 sudo apt install libcap-dev
-sudo apt-get install libssl-dev
-git clone https://github.com/IntelRealSense/librealsense
-cd librealsense
-mkdir build
-cd build
-cmake ../ -DBUILD_PYTHON_BINDINGS=TRUE
-make -j4
-sudo make install #Optional if you want the library to be installed in your system
-
-mkdir donkey_ws
-cd donkey_ws
+mkdir projects
+cd projects
 git clone https://github.com/autorope/donkeycar
 cd donkeycar
 git checkout main
 pip install -e .[pi]
-
+pip pybind11
 python -c "import tensorflow; print(tensorflow.__version__)"
+
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
+sudo apt-get install libssl-dev libusb-1.0-0-dev libudev-dev pkg-config libgtk-3-dev
+sudo apt-get install git wget cmake build-essential
+sudo apt-get install libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev at
+cd ~
+git clone https://github.com/IntelRealSense/librealsense.git
+cd librealsense
+sudo ./scripts/setup_udev_rules.sh
+
+mkdir build && cd build
+cmake ../
+make -j4
+sudo make install
 
 echo "alias ds='cd 	~/donkey_ws/donkeycar'" >> ~/.bashrc
 echo "alias dm='cd ~/mycar'" >> ~/.bashrc
